9cdaa2f42f5497a32f06a010cf34c79236363865
Jedidah <jedidahwambuguw@gmail.com>
2023-07-25 21:29:45 +0300

    Added 1. Block Scopewq

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,7 +1,8 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
+  // eslint-disable-next-line no-empty
   if (trueOrFalse) {
   }
 
