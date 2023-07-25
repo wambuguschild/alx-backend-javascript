80e51bf998e59335f3322e3106f092297762e7d8
Jedidah <jedidahwambuguw@gmail.com>
2023-07-25 22:45:04 +0300

    Added task 11 and 12

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,6 +1,6 @@
 export default function createReportObject(employeesList) {
   return {
-    allEmployees: employeesList,
+    allEmployees: { ...employeesList },
     getNumberOfDepartments: function () {
       return Object.keys(employeesList).length;
     },
