3dc942a2a91f299b935acdc58b60a048d0826d87
Jedidah <jedidahwambuguw@gmail.com>
2023-08-02 15:45:27 +0300

    Fixed task 2 error

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,10 @@
 export default function handleResponseFromAPI(promise) {
-   const body = { status: 200, body: 'success' };
-  
-    return promise
-      .then(() => body)
-      .catch(() => Error())
-      .finally(() => {
-        console.log('Got a response from the API');
-      });
-  }
+  const body = { status: 200, body: 'success' };
+
+  return promise
+    .then(() => body)
+    .catch(() => Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+}
