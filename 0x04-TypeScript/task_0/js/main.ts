5130ee792d4edc3eb3dcae83e93bfd14c8dee99e
Jedidah <jedidahwambuguw@gmail.com>
2023-08-04 21:26:02 +0300

    Added task 0

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,21 @@
+interface Student {
+	firstName: string,
+	lastName: string,
+	age: number,
+	location: string,
+{
+
+const [student1, student2] = [
+    {
+	firstName: "Zade",
+	lastName: "Meadows",
+	Age: 28,
+	loaction: "San-Diego",
+    },
+    {
+	firstName: "Adeline",
+	lastName: "Reilly",
+	Age: 25,
+	location: "Los-Angeles",
+     }
+]
