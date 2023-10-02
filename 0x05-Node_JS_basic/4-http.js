32a9cf66a246bf3610bb950e232a776fc611e86a
Jedidah <jedidahwambuguw@gmail.com>
2023-10-02 22:14:25 +0300

    Added task 5. Create a more complex HTTP server using Node_s HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,10 +1,16 @@
 const http = require('http');
 
+const hostname = '127.0.0.1';
+const port = 1245;
+
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Holberton School!\n');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
-app.listen(1245);
+app.listen(port, hostname, () => {
+  //   console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 module.exports = app;
