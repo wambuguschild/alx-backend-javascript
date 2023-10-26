66c22cf737c5821fe0ec8dbd226ef181d5a20f0d
Jedidah <jedidahwambuguw@gmail.com>
2023-10-26 23:06:42 +0300

    Fixed errors

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,7 +1,7 @@
 const readDatabase = require('../utils');
 
 class StudentsController {
-  static getAllStudents (request, response) {
+  static getAllStudents(request, response) {
     readDatabase(process.argv[2].toString()).then((students) => {
       const output = [];
       output.push('This is the list of our students');
@@ -16,7 +16,7 @@ class StudentsController {
     });
   }
 
-  static getAllStudentsByMajor (request, response) {
+  static getAllStudentsByMajor(request, response) {
     const field = request.params.major;
     readDatabase(process.argv[2].toString()).then((students) => {
       if (!(field in students)) {
